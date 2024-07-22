fd0ccee44e2f7305551c2abcd83f9fc61d8624ea
GRZ29 <rosalesgerald198@gmail.com>
2024-07-22 10:59:18 -0600

    feat(web): adding base Path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
 const nextConfig = {
+    basePath: '/nasa-api-view',
     output: "export",
     images:{
         unoptimized: envImageUnoptimize,
