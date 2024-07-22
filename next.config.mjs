94455e30d68ccfe09ed29d60b4185274eb98e924
GRZ29 <rosalesgerald198@gmail.com>
2024-07-22 00:02:07 -0600

    feat(web):last change before sleep

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
 const nextConfig = {
     output: "export",
-    // distDir: "dist",
+    distDir: "out",
     images:{
         unoptimized: envImageUnoptimize,
     }
