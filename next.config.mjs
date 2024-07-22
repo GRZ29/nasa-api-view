720981c21570d8aeeeaa75e20b0b6dfe10ee944d
GRZ29 <rosalesgerald198@gmail.com>
2024-07-21 23:46:26 -0600

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
 const nextConfig = {
-    output: process.env.NODE_ENV !== "production" ? undefined: "export",
-    distDir: "dist",
+    output: "export",
+    // distDir: "dist",
     images:{
         unoptimized: envImageUnoptimize,
     }
