1cb7b3c4a9516e698b29f74c7704e0ab73a86c5b
GRZ29 <rosalesgerald198@gmail.com>
2024-07-21 23:03:05 -0600

    fasdasd

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
     output: "export",
     distDir: "dist",
+    basePath: basePath,
+    assetPrefix: assetPrefix,
     images:{
         unoptimized: true,
     }
